46310667e8d567b7ea87551319e6b985501502ce
xelpixness <xelpixness@gmail.com>
2025-08-01 13:36:05 +0700

    ref

--- src/main.py
+++ src/main.py
@@ -3,31 +3,85 @@ import json
 from tabulate import tabulate
 from datetime import datetime
 
+from dataclasses import dataclass
+
+from abc import ABC, abstractmethod
+
 from arg_checks import check_file_access, check_date, check_report_type, AVAILABLE_REPORTS
+
+
+class ReportStrategy(ABC):
+    @abstractmethod
+    def grab_data(self, stats: dict, log_entry: dict): ...
+
+    @abstractmethod
+    def generate_report(self, stats: dict) -> list[dict]: ...
+
+
+@dataclass
+class AverageReportItem:
+    handler: str
+    total: int
+    avg_response_time: float
+
+
+class AverageReportStrategy(ReportStrategy):
+    def grab_data(self, stats: dict, log_entry: dict):
+        url = log_entry.get('url', 'unknown')
+        response_time = log_entry.get('response_time', 0.0)
+        if url not in stats:
+            stats[url] = {'total': 0, 'sum_response_time': 0.0}
+        stats[url]['total'] += 1
+        stats[url]['sum_response_time'] += response_time
+
+    def generate_report(self, stats: dict) -> list[AverageReportItem]:
+        report = []
+        for url, data in stats.items():
+            avg_time = data['sum_response_time'] / data['total'] if data['total'] > 0 else 0
+            report.append(AverageReportItem(
+                handler=url,
+                total=data['total'],
+                avg_response_time=round(avg_time, 3)
+            ))
+        return sorted(report, key=lambda x: x.total, reverse=True)
+
+
+
+class UserAgentStrategy(ReportStrategy):
+    def grab_data(self, stats: dict, log_entry: dict):
+        user_agent = log_entry.get('http_user_agent', 'unknown')
+        if user_agent not in stats:
+            stats[user_agent] = {'total': 0}
+        stats[user_agent]['total'] += 1
+
+
+    def generate_report(self, stats: dict) -> list[dict]:
+        report = []
+        for user_agent, data in stats.items():
+            report.append({
+                'user_agent': user_agent,
+                'total': data['total'],
+            })
+        return sorted(report, key=lambda x: x['total'], reverse=True)
+
 
 
 class LogAnalyzer:
     def __init__(self):
-        self.file_paths = []
-        self.available_reports = AVAILABLE_REPORTS
-        self.target_date = None
-        self.report_type = None
-        self._report_data = []
+        self.file_paths: list[str] = []
+        self.target_date: datetime.time = None
+        self._report_data: list[dict] = []
+        self.strategy: ReportStrategy | None = None
 
-        self._grab_methods = {
-            'average': self._grab_by_average,
-            'user-agent': self._grab_by_user_agent,
-            'max': self._grab_by_max # не реализован
-        }
 
-        self._report_generators = {
-            'average': self._generate_average_report,
-            'user-agent': self._generate_user_agent_report,
-            'max': self._generate_max_report, # не реализован
-        }
+    def set_strategy(self, strategy: ReportStrategy):
+        self.strategy = strategy
 
 
     def process_logs(self):
+        if not self.strategy:
+            raise ValueError("Не выбран тип отчета!")
+
         stats = {}
         for file_path in self.file_paths:
             with open(file_path, 'r') as file:
@@ -44,76 +98,21 @@ class LogAnalyzer:
                             if log_timestamp != self.target_date:
                                 continue
 
-                        self._grab_methods[self.report_type](stats, log_entry)
+                        self.strategy.grab_data(stats, log_entry)
 
                     except (json.JSONDecodeError, ValueError) as e:
                         print(f"Ошибка в строке файла {file_path}: {line.strip()}")
                         print(f"Ошибка: {str(e)}")
 
-        self._report_generators[self.report_type](stats)
+        self._report_data = self.strategy.generate_report(stats)
 
 
-
-    def _grab_by_average(self, stats, log_entry):
-        url = log_entry.get('url', 'unknown')
-        response_time = log_entry.get('response_time', 0.0)
-
-        if url not in stats:
-            stats[url] = {'total': 0, 'sum_response_time': 0.0}
-
-        stats[url]['total'] += 1
-        stats[url]['sum_response_time'] += response_time
-
-
-    def _generate_average_report(self, stats):
-        report = []
-        for url, data in stats.items():
-            avg_time = data['sum_response_time'] / data['total'] if data['total'] > 0 else 0
-            report.append({
-                'handler': url,
-                'total': data['total'],
-                'avg_response_time': round(avg_time, 3)
-            })
-        self._report_data = report
-        self._report_data.sort(key=lambda x: x['total'], reverse=True)
-
-
-    # Бонусное
-    def _grab_by_user_agent(self, stats, log_entry):
-        user_agent = log_entry.get('http_user_agent', 'unknown')
-        if user_agent not in stats:
-            stats[user_agent] = {'total': 0}
-        stats[user_agent]['total'] += 1
-
-
-    def _generate_user_agent_report(self, stats):
-        report = []
-        for user_agent, data in stats.items():
-            report.append({
-                'user_agent': user_agent,
-                'total': data['total'],
-            })
-        self._report_data = report
-        self._report_data.sort(key=lambda x: x['total'], reverse=True)
-
-
-    # если надо пропишем здесь логику сбора данных
-    # а в ф-ии ниже сгенерим отчет. пока пусть выбрасывает необработанное исключение
-    def _grab_by_max(self, stats, log_entry):
-        raise NotImplementedError("Отчёт 'max' в разработке")
-    def _generate_max_report(self, stats):
-        ...
-
-
-    def print_report(self):
+    def print_report(self) -> None:
         print(tabulate(self._report_data, headers='keys', showindex='always'))
 
-    def get_available_reports(self):
-        return self.available_reports
 
-    def __str__(self):
-        return f"LogAnalyzer (report_type={self.report_type}, files={len(self.file_paths)})"
-
+    def __str__(self) -> str:
+        return f"LogAnalyzer, files={len(self.file_paths)})"
 
 
 def main():
@@ -140,7 +139,12 @@ def main():
 
     log_analyzer.file_paths = args.file
     log_analyzer.target_date = args.date
-    log_analyzer.report_type = args.report
+
+
+    if args.report == "average":
+        log_analyzer.set_strategy(AverageReportStrategy())
+    elif args.report == "user-agent":
+        log_analyzer.set_strategy(UserAgentStrategy())
 
     log_analyzer.process_logs()
     log_analyzer.print_report()
@@ -148,6 +152,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
